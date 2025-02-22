31220bd5800656e3ccfd0571816ddc73bd94dd07
Hammanjee <hammanjee@gmail.com>
2025-02-23 00:02:10 +0500

    Add files via upload

---
+++ old.py
@@ -0,0 +1,9 @@
+# a=20
+# b=5
+# def arith_funcation(a:int,b:int):
+#     print(f'Addition of {a} and {b} is {a+b}') 
+#     print(f' Subtraction of {a} and {b} is {a-b}')
+#     print(f' Mulltiplication of {a} and {b} is {a*b}')
+#     print (f' Division of {a} and {b} is {a/b}')
+# arith_funcation(20,5)
+
